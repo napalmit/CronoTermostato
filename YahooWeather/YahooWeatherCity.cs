4df5506bc87c3021db23713115f85169ddc195ea
napalmit <luigi.piccione@gmail.com>
2015-04-04 19:03:16 +0200

    hhh

---
+++ YahooWeather/YahooWeatherCity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahooWeatherDLL
+{
+    public class YahooWeatherCity
+    {
+        public YahooWeatherCity() { }
+
+        public string CITY { get; set; }
+
+        public string REGION { get; set; }
+
+        public string COUNTRY { get; set; }
+
+        public string TEMPERATURE { get; set; }
+
+        public string CHILL { get; set; }
+
+        public string HUMIDITY { get; set; }
+
+        public string SUNRISE { get; set; }
+
+        public string SUNSET { get; set; }
+
+        public List<Previsione> LISTA_PREVISIONI { get; set; }
+
+        public string CODE { get; set; }
+
+        public string TEMPERATURE_UNIT { get; set; }
+
+        public string IMG { get; set; }
+    }
+
+    public class Previsione
+    {
+        public Previsione() { }
+
+        public string GIORNO { get; set; }
+
+        public string DATA { get; set; }
+
+        public string CODE { get; set; }
+
+        public string IMG { get; set; }
+    }
+}
